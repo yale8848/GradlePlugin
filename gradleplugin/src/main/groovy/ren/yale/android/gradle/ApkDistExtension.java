4c2dcb373d6eb428904ab3c85a959e5a90efff85
yale8848 <royal8848@163.com>
2017-12-17 22:56:16 +0800

    update

---
+++ gradleplugin/src/main/groovy/ren/yale/android/gradle/ApkDistExtension.java
@@ -0,0 +1,12 @@
+package ren.yale.android.gradle;
+
+import groovy.lang.Closure;
+
+/**
+ * Created by yale on 2017/12/17.
+ */
+
+public class ApkDistExtension {
+    Closure nameMap = null;
+    String destDir = null;
+}
---
+++ gradleplugin/src/main/groovy/ren/yale/android/gradle/MyPlugin.groovy
@@ -0,0 +1,22 @@
+package ren.yale.android.gralde
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+//http://xuyushi.github.io/2017/06/03/gradle%20%E6%8F%92%E4%BB%B6%E5%BC%80%E5%8F%91/
+public class MyPlugin implements Plugin<Project>{
+
+    @Override
+    void apply(Project project) {
+
+        project.task("apkdist") <<{
+            //project.extensions.create('apkdistconf', ApkDistExtension)
+
+            //def closure = project['apkdistconf'].nameMap
+            //println closure('wow!');
+            //println project['apkdistconf'].destDir
+
+            println "hello ,world"
+        }
+
+    }
+}
